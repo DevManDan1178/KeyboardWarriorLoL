19f9202d5a9e97801b7e5eba743038e83d9b4d64
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-02 00:13:15 -0400

    + refactored hotkey structs and enums into own hpp

---
+++ src/input/Hotkey.hpp
@@ -0,0 +1,22 @@
+#pragma once
+enum class BindType {
+    Keyboard, Mouse, None
+};
+
+enum Modifiers {
+    None = 0,
+    Shift = 1 << 0,
+    // Ctrl = 1 << 1,
+    // Alt = 1 << 2,
+};
+
+struct Hotkey {
+    int keyCode;
+    BindType bindType;
+    uint8_t modifiers; //Bitmask from enum Modifiers
+    Hotkey()
+        : keyCode(0), bindType(BindType::None), modifiers(0) {}
+        
+    Hotkey(int k, BindType b, uint8_t m)
+        : keyCode(k), bindType(b), modifiers(m) {}
+};
